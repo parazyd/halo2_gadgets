zcash/orchard

ecc: Merge FixedPoints, FixedPointsBaseField, FixedPointsShort types.
This API change defines a FixedPoints trait that is implemented for
all OrchardFixedBases. This makes EccChip, SinsemillaChip, and
MerkleChip generic over the fixed bases used.

src/circuit/gadget/sinsemilla/chip.rs
@@ -4,17 +4,14 @@ use super::{
 };
 use crate::{
     circuit::gadget::{
-        ecc::chip::EccPoint,
+        ecc::{chip::EccPoint, FixedPoints},
         utilities::{lookup_range_check::LookupRangeCheckConfig, CellValue, Var},
     },
-    constants::OrchardFixedBasesFull,
-    primitives::sinsemilla::{
-        self, Q_COMMIT_IVK_M_GENERATOR, Q_MERKLE_CRH, Q_NOTE_COMMITMENT_M_GENERATOR,
-    },
+    primitives::sinsemilla,
 };
+use std::marker::PhantomData;
 
 use halo2::{
-    arithmetic::{CurveAffine, FieldExt},
     circuit::{Chip, Layouter},
     plonk::{
         Advice, Column, ConstraintSystem, Error, Expression, Fixed, Selector, TableColumn,
@@ -22,7 +19,7 @@ use halo2::{
     },
     poly::Rotation,
 };
-use pasta_curves::pallas;
+use pasta_curves::{arithmetic::FieldExt, pallas};
 
 mod generator_table;
 use generator_table::GeneratorTableConfig;
@@ -31,7 +28,12 @@ mod hash_to_point;
 
 /// Configuration for the Sinsemilla hash chip
 #[derive(Eq, PartialEq, Clone, Debug)]
-pub struct SinsemillaConfig {
+pub struct SinsemillaConfig<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     /// Binary selector used in lookup argument and in the body of the Sinsemilla hash.
     q_sinsemilla1: Selector,
     /// Non-binary selector used in lookup argument and in the body of the Sinsemilla hash.
@@ -66,9 +68,15 @@ pub struct SinsemillaConfig {
     generator_table: GeneratorTableConfig,
     /// An advice column configured to perform lookup range checks.
     lookup_config: LookupRangeCheckConfig<pallas::Base, { sinsemilla::K }>,
+    _marker: PhantomData<(Hash, Commit, F)>,
 }
 
-impl SinsemillaConfig {
+impl<Hash, Commit, F> SinsemillaConfig<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     /// Returns an array of all advice columns in this config, in arbitrary order.
     pub fn advices(&self) -> [Column<Advice>; 5] {
         [self.x_a, self.x_p, self.bits, self.lambda_1, self.lambda_2]
@@ -81,12 +89,22 @@ impl SinsemillaConfig {
 }
 
 #[derive(Eq, PartialEq, Clone, Debug)]
-pub struct SinsemillaChip {
-    config: SinsemillaConfig,
+pub struct SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Fixed: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+{
+    config: SinsemillaConfig<Hash, Commit, Fixed>,
 }
 
-impl Chip<pallas::Base> for SinsemillaChip {
-    type Config = SinsemillaConfig;
+impl<Hash, Commit, Fixed> Chip<pallas::Base> for SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Fixed: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+{
+    type Config = SinsemillaConfig<Hash, Commit, Fixed>;
     type Loaded = ();
 
     fn config(&self) -> &Self::Config {
@@ -98,13 +116,18 @@ impl Chip<pallas::Base> for SinsemillaChip {
     }
 }
 
-impl SinsemillaChip {
+impl<Hash, Commit, F> SinsemillaChip<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     pub fn construct(config: <Self as Chip<pallas::Base>>::Config) -> Self {
         Self { config }
     }
 
     pub fn load(
-        config: SinsemillaConfig,
+        config: SinsemillaConfig<Hash, Commit, F>,
         layouter: &mut impl Layouter<pallas::Base>,
     ) -> Result<<Self as Chip<pallas::Base>>::Loaded, Error> {
         // Load the lookup table.
@@ -129,7 +152,7 @@ impl SinsemillaChip {
             meta.enable_equality((*advice).into())
         }
 
-        let config = SinsemillaConfig {
+        let config = SinsemillaConfig::<Hash, Commit, F> {
             q_sinsemilla1: meta.complex_selector(),
             q_sinsemilla2: meta.fixed_column(),
             q_sinsemilla4: meta.selector(),
@@ -146,6 +169,7 @@ impl SinsemillaChip {
                 table_y: lookup.2,
             },
             lookup_config: range_check,
+            _marker: PhantomData,
         };
 
         // Set up lookup argument
@@ -241,8 +265,12 @@ impl SinsemillaChip {
 }
 
 // Implement `SinsemillaInstructions` for `SinsemillaChip`
-impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }>
-    for SinsemillaChip
+impl<Hash, Commit, F> SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }>
+    for SinsemillaChip<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
     type CellValue = CellValue<pallas::Base>;
 
@@ -253,10 +281,10 @@ impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }
 
     type X = CellValue<pallas::Base>;
     type Point = EccPoint;
-    type FixedPoints = OrchardFixedBasesFull;
+    type FixedPoints = F;
 
-    type HashDomains = SinsemillaHashDomains;
-    type CommitDomains = SinsemillaCommitDomains;
+    type HashDomains = Hash;
+    type CommitDomains = Commit;
 
     fn witness_message_piece(
         &self,
@@ -296,59 +324,5 @@ impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }
 
     fn extract(point: &Self::Point) -> Self::X {
         point.x()
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum SinsemillaHashDomains {
-    NoteCommit,
-    CommitIvk,
-    MerkleCrh,
-}
-
-#[allow(non_snake_case)]
-impl HashDomains<pallas::Affine> for SinsemillaHashDomains {
-    fn Q(&self) -> pallas::Affine {
-        match self {
-            SinsemillaHashDomains::CommitIvk => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_COMMIT_IVK_M_GENERATOR.0).unwrap(),
-                pallas::Base::from_bytes(&Q_COMMIT_IVK_M_GENERATOR.1).unwrap(),
-            )
-            .unwrap(),
-            SinsemillaHashDomains::NoteCommit => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_NOTE_COMMITMENT_M_GENERATOR.0).unwrap(),
-                pallas::Base::from_bytes(&Q_NOTE_COMMITMENT_M_GENERATOR.1).unwrap(),
-            )
-            .unwrap(),
-            SinsemillaHashDomains::MerkleCrh => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_MERKLE_CRH.0).unwrap(),
-                pallas::Base::from_bytes(&Q_MERKLE_CRH.1).unwrap(),
-            )
-            .unwrap(),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum SinsemillaCommitDomains {
-    NoteCommit,
-    CommitIvk,
-}
-
-impl CommitDomains<pallas::Affine, OrchardFixedBasesFull, SinsemillaHashDomains>
-    for SinsemillaCommitDomains
-{
-    fn r(&self) -> OrchardFixedBasesFull {
-        match self {
-            Self::NoteCommit => OrchardFixedBasesFull::NoteCommitR,
-            Self::CommitIvk => OrchardFixedBasesFull::CommitIvkR,
-        }
-    }
-
-    fn hash_domain(&self) -> SinsemillaHashDomains {
-        match self {
-            Self::NoteCommit => SinsemillaHashDomains::NoteCommit,
-            Self::CommitIvk => SinsemillaHashDomains::CommitIvk,
-        }
     }
 }

src/circuit/gadget/sinsemilla/merkle.rs
@@ -139,10 +139,13 @@ pub mod tests {
 
     use crate::{
         circuit::gadget::{
-            sinsemilla::chip::{SinsemillaChip, SinsemillaHashDomains},
+            sinsemilla::chip::SinsemillaChip,
             utilities::{lookup_range_check::LookupRangeCheckConfig, UtilitiesInstructions, Var},
         },
-        constants::{L_ORCHARD_BASE, MERKLE_CRH_PERSONALIZATION, MERKLE_DEPTH_ORCHARD},
+        constants::{
+            OrchardCommitDomains, OrchardFixedBases, OrchardHashDomains, L_ORCHARD_BASE,
+            MERKLE_CRH_PERSONALIZATION, MERKLE_DEPTH_ORCHARD,
+        },
         primitives::sinsemilla::HashDomain,
         spec::i2lebsp,
     };
@@ -167,7 +170,10 @@ pub mod tests {
     }
 
     impl Circuit<pallas::Base> for MyCircuit {
-        type Config = (MerkleConfig, MerkleConfig);
+        type Config = (
+            MerkleConfig<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>,
+            MerkleConfig<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>,
+        );
         type FloorPlanner = SimpleFloorPlanner;
 
         fn without_witnesses(&self) -> Self {
@@ -235,7 +241,10 @@ pub mod tests {
             mut layouter: impl Layouter<pallas::Base>,
         ) -> Result<(), Error> {
             // Load generator table (shared across both configs)
-            SinsemillaChip::load(config.0.sinsemilla_config.clone(), &mut layouter)?;
+            SinsemillaChip::<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>::load(
+                config.0.sinsemilla_config.clone(),
+                &mut layouter,
+            )?;
 
             // Construct Merkle chips which will be placed side-by-side in the circuit.
             let chip_1 = MerkleChip::construct(config.0.clone());
@@ -250,7 +259,7 @@ pub mod tests {
             let path = MerklePath {
                 chip_1,
                 chip_2,
-                domain: SinsemillaHashDomains::MerkleCrh,
+                domain: OrchardHashDomains::MerkleCrh,
                 leaf_pos: self.leaf_pos,
                 path: self.merkle_path,
             };

parazyd/halo2_gadgets

src/circuit/gadget/sinsemilla/chip.rs
@@ -4,17 +4,14 @@ use super::{
 };
 use crate::{
     circuit::gadget::{
-        ecc::chip::NonIdentityEccPoint,
+        ecc::{chip::NonIdentityEccPoint, FixedPoints},
         utilities::{lookup_range_check::LookupRangeCheckConfig, CellValue, Var},
     },
-    constants::OrchardFixedBasesFull,
-    primitives::sinsemilla::{
-        self, Q_COMMIT_IVK_M_GENERATOR, Q_MERKLE_CRH, Q_NOTE_COMMITMENT_M_GENERATOR,
-    },
+    primitives::sinsemilla,
 };
+use std::marker::PhantomData;
 
 use halo2::{
-    arithmetic::{CurveAffine, FieldExt},
     circuit::{Chip, Layouter},
     plonk::{
         Advice, Column, ConstraintSystem, Error, Expression, Fixed, Selector, TableColumn,
@@ -22,7 +19,7 @@ use halo2::{
     },
     poly::Rotation,
 };
-use pasta_curves::pallas;
+use pasta_curves::{arithmetic::FieldExt, pallas};
 
 mod generator_table;
 use generator_table::GeneratorTableConfig;
@@ -31,7 +28,12 @@ mod hash_to_point;
 
 /// Configuration for the Sinsemilla hash chip
 #[derive(Eq, PartialEq, Clone, Debug)]
-pub struct SinsemillaConfig {
+pub struct SinsemillaConfig<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     /// Binary selector used in lookup argument and in the body of the Sinsemilla hash.
     q_sinsemilla1: Selector,
     /// Non-binary selector used in lookup argument and in the body of the Sinsemilla hash.
@@ -66,9 +68,15 @@ pub struct SinsemillaConfig {
     generator_table: GeneratorTableConfig,
     /// An advice column configured to perform lookup range checks.
     lookup_config: LookupRangeCheckConfig<pallas::Base, { sinsemilla::K }>,
+    _marker: PhantomData<(Hash, Commit, F)>,
 }
 
-impl SinsemillaConfig {
+impl<Hash, Commit, F> SinsemillaConfig<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     /// Returns an array of all advice columns in this config, in arbitrary order.
     pub fn advices(&self) -> [Column<Advice>; 5] {
         [self.x_a, self.x_p, self.bits, self.lambda_1, self.lambda_2]
@@ -81,12 +89,22 @@ impl SinsemillaConfig {
 }
 
 #[derive(Eq, PartialEq, Clone, Debug)]
-pub struct SinsemillaChip {
-    config: SinsemillaConfig,
+pub struct SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Fixed: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+{
+    config: SinsemillaConfig<Hash, Commit, Fixed>,
 }
 
-impl Chip<pallas::Base> for SinsemillaChip {
-    type Config = SinsemillaConfig;
+impl<Hash, Commit, Fixed> Chip<pallas::Base> for SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Fixed: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+{
+    type Config = SinsemillaConfig<Hash, Commit, Fixed>;
     type Loaded = ();
 
     fn config(&self) -> &Self::Config {
@@ -98,13 +116,18 @@ impl Chip<pallas::Base> for SinsemillaChip {
     }
 }
 
-impl SinsemillaChip {
+impl<Hash, Commit, F> SinsemillaChip<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
+{
     pub fn construct(config: <Self as Chip<pallas::Base>>::Config) -> Self {
         Self { config }
     }
 
     pub fn load(
-        config: SinsemillaConfig,
+        config: SinsemillaConfig<Hash, Commit, F>,
         layouter: &mut impl Layouter<pallas::Base>,
     ) -> Result<<Self as Chip<pallas::Base>>::Loaded, Error> {
         // Load the lookup table.
@@ -129,7 +152,7 @@ impl SinsemillaChip {
             meta.enable_equality((*advice).into())
         }
 
-        let config = SinsemillaConfig {
+        let config = SinsemillaConfig::<Hash, Commit, F> {
             q_sinsemilla1: meta.complex_selector(),
             q_sinsemilla2: meta.fixed_column(),
             q_sinsemilla4: meta.selector(),
@@ -146,6 +169,7 @@ impl SinsemillaChip {
                 table_y: lookup.2,
             },
             lookup_config: range_check,
+            _marker: PhantomData,
         };
 
         // Set up lookup argument
@@ -241,8 +265,12 @@ impl SinsemillaChip {
 }
 
 // Implement `SinsemillaInstructions` for `SinsemillaChip`
-impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }>
-    for SinsemillaChip
+impl<Hash, Commit, F> SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }>
+    for SinsemillaChip<Hash, Commit, F>
+where
+    Hash: HashDomains<pallas::Affine>,
+    F: FixedPoints<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
     type CellValue = CellValue<pallas::Base>;
 
@@ -253,10 +281,10 @@ impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }
 
     type X = CellValue<pallas::Base>;
     type NonIdentityPoint = NonIdentityEccPoint;
-    type FixedPoints = OrchardFixedBasesFull;
+    type FixedPoints = F;
 
-    type HashDomains = SinsemillaHashDomains;
-    type CommitDomains = SinsemillaCommitDomains;
+    type HashDomains = Hash;
+    type CommitDomains = Commit;
 
     fn witness_message_piece(
         &self,
@@ -296,59 +324,5 @@ impl SinsemillaInstructions<pallas::Affine, { sinsemilla::K }, { sinsemilla::C }
 
     fn extract(point: &Self::NonIdentityPoint) -> Self::X {
         point.x()
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum SinsemillaHashDomains {
-    NoteCommit,
-    CommitIvk,
-    MerkleCrh,
-}
-
-#[allow(non_snake_case)]
-impl HashDomains<pallas::Affine> for SinsemillaHashDomains {
-    fn Q(&self) -> pallas::Affine {
-        match self {
-            SinsemillaHashDomains::CommitIvk => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_COMMIT_IVK_M_GENERATOR.0).unwrap(),
-                pallas::Base::from_bytes(&Q_COMMIT_IVK_M_GENERATOR.1).unwrap(),
-            )
-            .unwrap(),
-            SinsemillaHashDomains::NoteCommit => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_NOTE_COMMITMENT_M_GENERATOR.0).unwrap(),
-                pallas::Base::from_bytes(&Q_NOTE_COMMITMENT_M_GENERATOR.1).unwrap(),
-            )
-            .unwrap(),
-            SinsemillaHashDomains::MerkleCrh => pallas::Affine::from_xy(
-                pallas::Base::from_bytes(&Q_MERKLE_CRH.0).unwrap(),
-                pallas::Base::from_bytes(&Q_MERKLE_CRH.1).unwrap(),
-            )
-            .unwrap(),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum SinsemillaCommitDomains {
-    NoteCommit,
-    CommitIvk,
-}
-
-impl CommitDomains<pallas::Affine, OrchardFixedBasesFull, SinsemillaHashDomains>
-    for SinsemillaCommitDomains
-{
-    fn r(&self) -> OrchardFixedBasesFull {
-        match self {
-            Self::NoteCommit => OrchardFixedBasesFull::NoteCommitR,
-            Self::CommitIvk => OrchardFixedBasesFull::CommitIvkR,
-        }
-    }
-
-    fn hash_domain(&self) -> SinsemillaHashDomains {
-        match self {
-            Self::NoteCommit => SinsemillaHashDomains::NoteCommit,
-            Self::CommitIvk => SinsemillaHashDomains::CommitIvk,
-        }
     }
 }

src/circuit/gadget/sinsemilla/merkle.rs
@@ -139,10 +139,12 @@ pub mod tests {
 
     use crate::{
         circuit::gadget::{
-            sinsemilla::chip::{SinsemillaChip, SinsemillaHashDomains},
+            sinsemilla::chip::SinsemillaChip,
             utilities::{lookup_range_check::LookupRangeCheckConfig, UtilitiesInstructions, Var},
         },
-        constants::MERKLE_DEPTH_ORCHARD,
+        constants::{
+            OrchardCommitDomains, OrchardFixedBases, OrchardHashDomains, MERKLE_DEPTH_ORCHARD,
+        },
         note::commitment::ExtractedNoteCommitment,
         tree,
     };
@@ -166,7 +168,10 @@ pub mod tests {
     }
 
     impl Circuit<pallas::Base> for MyCircuit {
-        type Config = (MerkleConfig, MerkleConfig);
+        type Config = (
+            MerkleConfig<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>,
+            MerkleConfig<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>,
+        );
         type FloorPlanner = SimpleFloorPlanner;
 
         fn without_witnesses(&self) -> Self {
@@ -234,7 +239,10 @@ pub mod tests {
             mut layouter: impl Layouter<pallas::Base>,
         ) -> Result<(), Error> {
             // Load generator table (shared across both configs)
-            SinsemillaChip::load(config.0.sinsemilla_config.clone(), &mut layouter)?;
+            SinsemillaChip::<OrchardHashDomains, OrchardCommitDomains, OrchardFixedBases>::load(
+                config.0.sinsemilla_config.clone(),
+                &mut layouter,
+            )?;
 
             // Construct Merkle chips which will be placed side-by-side in the circuit.
             let chip_1 = MerkleChip::construct(config.0.clone());
@@ -249,7 +257,7 @@ pub mod tests {
             let path = MerklePath {
                 chip_1,
                 chip_2,
-                domain: SinsemillaHashDomains::MerkleCrh,
+                domain: OrchardHashDomains::MerkleCrh,
                 leaf_pos: self.leaf_pos,
                 path: self.merkle_path,
             };